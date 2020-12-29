0c0e4ba91c51443e4aefb07371c5bd2587f021e9
Gerrit Toehgiono <gerrit.toehgiono@rwth-aachen.de>
2020-12-29 13:15:18 +0100

    Disable media.peerconnection.enabled "false" due to breakage.

--- user.js
+++ user.js
@@ -79,7 +79,8 @@ user_pref("dom.network.enabled",				false);
 
 // PREF: Disable WebRTC entirely to prevent leaking internal IP addresses (Firefox < 42)
 // NOTICE: Disabling WebRTC breaks peer-to-peer file sharing tools (reep.io ...)
-user_pref("media.peerconnection.enabled",			false);
+// NOTICE-DISABLED: Breaks Threema Web and onther online IM interfaces
+//user_pref("media.peerconnection.enabled",			false);
 
 // PREF: Don't reveal your internal IP when WebRTC is enabled (Firefox >= 42)
 // https://wiki.mozilla.org/Media/WebRTC/Privacy
